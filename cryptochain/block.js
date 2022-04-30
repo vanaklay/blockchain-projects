a19217d0ca3a05dcb838671cb312b00f489534af
vanaklay <ven2kaze@gmail.com>
2022-04-30 18:48:19 +0200

    add hash in minedBlock

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,4 +1,5 @@
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Block {
   constructor({ timestamp, lastHash, hash, data }) {
@@ -13,10 +14,13 @@ class Block {
   }
 
   static minedBlock({ lastBlock, data }) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
     return new this({
-      timestamp: Date.now(),
-      lastHash: lastBlock.hash,
+      timestamp,
+      lastHash,
       data,
+      hash: cryptoHash(timestamp, lastHash, data),
     });
   }
 }
